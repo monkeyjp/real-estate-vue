7053ee55214124cdf54b0ae453b883eecc984b83
Javier Pestana <javier.pestana85@gmail.com>
2024-10-10 22:18:39 +0100

    LoginView view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterView />
+  <v-card elevation="3" max-width="1200" class="mx-auto">
+    <v-layout>
+      <v-app-bar color="blue-darken-1">
+        <template v-slot:prepend>
+          <v-btn :to="{ name: 'home' }"> Real Estate - VueFire </v-btn>
+        </template>
+        <template v-slot:append>
+          <v-btn :to="{ name: 'home' }">Home</v-btn>
+          <v-btn :to="{ name: 'login' }">Login</v-btn>
+        </template>
+      </v-app-bar>
+      <v-main>
+        <v-container>
+          <RouterView />
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <style scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <v-card flat max-width="600" class="mx-auto my-10">
+    <v-card-tittle class="text-h4 font-weight-bold" tag="h3"
+      >Login</v-card-tittle
+    >
+    <v-form class="mt-5">
+      <v-text-field type="email" label="Email" bg-color="blue-grey-lighten-5" />
+      <v-text-field
+        type="password"
+        label="Password"
+        bg-color="blue-grey-lighten-5"
+      />
+      <v-btn color="pink-accent-3" block>Login</v-btn>
+    </v-form>
+  </v-card>
+</template>
