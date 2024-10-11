8115bccfa9b67b4a19da095a9e0b6d72501afec6
Javier Pestana <javier.pestana85@gmail.com>
2024-10-11 17:00:29 +0100

    validation schema for login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,39 @@
-<script setup></script>
+<script setup>
+import { useForm, useField } from "vee-validate";
+import { loginSchema as validationSchema } from "@/validation/loginSchema";
+
+const { handleSubmit } = useForm({ validationSchema });
+const email = useField("email");
+const password = useField("password");
+
+const submit = handleSubmit(() => {
+  console.log("...sumiting");
+});
+</script>
 
 <template>
   <v-card flat max-width="600" class="mx-auto my-10">
-    <v-card-tittle class="text-h4 font-weight-bold" tag="h3"
-      >Login</v-card-tittle
-    >
+    <v-card-title class="text-h4 font-weight-bold" tag="h3">
+      Login
+    </v-card-title>
     <v-form class="mt-5">
-      <v-text-field type="email" label="Email" bg-color="blue-grey-lighten-5" />
+      <v-text-field
+        type="email"
+        label="Email"
+        bg-color="blue-grey-lighten-5"
+        class="mb-3"
+        v-model="email.value.value"
+        :error-messages="email.errorMessage.value"
+      />
       <v-text-field
         type="password"
         label="Password"
         bg-color="blue-grey-lighten-5"
+        class="mb-3"
+        v-model="password.value.value"
+        :error-messages="password.errorMessage.value"
       />
-      <v-btn color="pink-accent-3" block>Login</v-btn>
+      <v-btn color="pink-accent-3" block @click="submit">Login</v-btn>
     </v-form>
   </v-card>
 </template>
