6b5ee2cdee38b880890e2656b241b650471c4a84
Javier Pestana <javier.pestana85@gmail.com>
2024-10-12 15:03:23 +0100

    install firebase vuefire, create useAuhStore

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+
+//Firebase
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './config/firebase'
+
 import App from './App.vue'
 import router from './router'
 
@@ -18,6 +23,12 @@ const app = createApp(App)
 const vuetify = createVuetify({
     components,
     directives
+})
+
+//Firebase
+app.use(VueFire, {
+    firebaseApp,
+    modules: [VueFireAuth()]
 })
 
 app.use(vuetify)
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { useForm, useField } from "vee-validate";
 import { loginSchema as validationSchema } from "@/validation/loginSchema";
+import { useAuthStore } from "@/stores/auth";
 
 const { handleSubmit } = useForm({ validationSchema });
+const auth = useAuthStore();
+
 const email = useField("email");
 const password = useField("password");
 
-const submit = handleSubmit(() => {
-  console.log("...sumiting");
+const submit = handleSubmit((values) => {
+  auth.login(values);
 });
 </script>
 
