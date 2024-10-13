4bb1e0d3377c678263fcfc5ecc32d0e78bd7c36d
Javier Pestana <javier.pestana85@gmail.com>
2024-10-13 21:46:28 +0100

    logout function

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { RouterView } from "vue-router";
+import { useAuthStore } from "./stores/auth";
+
+const auth = useAuthStore();
 </script>
 
 <template>
@@ -10,8 +13,14 @@ import { RouterView } from "vue-router";
           <v-btn :to="{ name: 'home' }"> Real Estate - VueFire </v-btn>
         </template>
         <template v-slot:append>
-          <v-btn :to="{ name: 'home' }">Home</v-btn>
-          <v-btn :to="{ name: 'login' }">Login</v-btn>
+          <div v-if="auth.isAuth">
+            <v-btn :to="{ name: 'admin-properties' }">Admin</v-btn>
+            <v-btn @click="auth.logout">Logout</v-btn>
+          </div>
+          <div v-else>
+            <v-btn :to="{ name: 'home' }">Home</v-btn>
+            <v-btn :to="{ name: 'login' }">Login</v-btn>
+          </div>
         </template>
       </v-app-bar>
       <v-main>
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,18 +1,28 @@
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { defineStore } from "pinia";
 import { useFirebaseAuth } from "vuefire";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { useRouter } from "vue-router";
 
 export const useAuthStore = defineStore('auth', () => {
 
     const auth = useFirebaseAuth();
-    const authUser = ref({})
+    const authUser = ref(null)
+    const router = useRouter()
 
     const errorMsg = ref('')
 
     const errorCodes = {
         "auth/invalid-credential": "Invalid email or password. Please try again."
     }
+
+    onMounted(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                authUser.value = user
+            }
+        })
+    })
 
     const login = ({ email, password }) => {
         signInWithEmailAndPassword(auth, email, password)
@@ -22,7 +32,7 @@ export const useAuthStore = defineStore('auth', () => {
                 }
                 const user = userCredential.user
                 authUser.value = user
-                console.log(authUser.value);
+                router.push({ name: "admin-properties" })
 
             })
             .catch((error) => {
@@ -32,11 +42,23 @@ export const useAuthStore = defineStore('auth', () => {
             });
     }
 
+    const logout = () => {
+        signOut(auth).then(() => {
+            authUser.value = null
+            router.push({ name: "login" })
+        }).catch(error => console.log(error))
+
+    }
+
     const hasError = computed(() => {
         return errorMsg.value
     })
 
+    const isAuth = computed(() => {
+        return authUser.value
+    })
+
     return {
-        login, hasError, errorMsg
+        login, hasError, errorMsg, isAuth, logout
     }
 })
