59650ff6340fa5fb70388aba4471098437e125fd
Javier Pestana <javier.pestana85@gmail.com>
2024-10-22 17:30:50 +0100

    home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,20 @@
-<script setup></script>
+<script setup>
+import useProperties from "@/composables/useProperties";
+import Property from "@/components/Property.vue";
+
+const { propertiesCollection, propertyPrice } = useProperties();
+</script>
 
 <template>
-  <main>
-    <h1 class="text-center text-h3 font-weight-bold my-5">Vue Real Estate</h1>
-  </main>
+  <h1 class="text-center text-h3 font-weight-bold my-5">Vue Real Estate</h1>
+  <v-card class="py-10" flat>
+    <v-row>
+      <Property
+        v-for="property in propertiesCollection"
+        :key="property.id"
+        :property="property"
+        :price="propertyPrice"
+      />
+    </v-row>
+  </v-card>
 </template>
